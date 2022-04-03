c692c65cf3e7bb26b41335b06d897664b6800a6d
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-03 14:49:33 +0530

    add EmployeeWage UC11

---
+++ src/Day9/EmployeeWageMain.java
@@ -0,0 +1,14 @@
+package Day9;
+
+public class EmployeeWageMain 
+{
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee wage Computation Program");
+		 IComputeEmpWage empWageBuilder=new EmpWageBuilder();
+		
+		empWageBuilder .addCompanyEmpWage("Dmart",20,2,10);
+		empWageBuilder .addCompanyEmpWage("Tata",20,4,20);
+		empWageBuilder.computeEmpWage();
+	}
+
+}
