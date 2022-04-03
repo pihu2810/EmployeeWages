7cc2277f61f4f61339f396f36570361b124caeb7
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-03 16:41:20 +0530

    add EmployeeWage UC14

--- src/Day9/CompanyEmpWage.java
+++ src/Day9/CompanyEmpWage.java
@@ -6,19 +6,20 @@ public class CompanyEmpWage
 	public final int Total_Working_Days;
 	public final int Total_Working_hours;
 	public final int Per_Hour_Salary;
-	public  int totalEmoWage;
+	public  int totalEmpWage;
 	
 	public CompanyEmpWage (String CompanyName, int Total_Working_Days, int Per_Hour_Salary,int Total_Working_hours) {
 		this.CompanyName = CompanyName;
 		this.Total_Working_Days = Total_Working_Days;
 		this.Per_Hour_Salary = Per_Hour_Salary;
 		this.Total_Working_hours=Total_Working_hours;
+		totalEmpWage=0;
 	}
-	public void setTotalEmpWage(int totalEmoWage) {
-		this.totalEmoWage=totalEmoWage;
+	public void setTotalEmpWage(int totalEmpWage) {
+		this.totalEmpWage=totalEmpWage;
 	}
  public  String toString() {
 		 
-		 return "Total employee Salary for Company: " + CompanyName + " is :" +totalEmoWage;
+		 return "Total employee Salary for Company: " + CompanyName + " is :" +totalEmpWage;
 	 }
 }
