36a3051b3dbe5d27134edeee555da5a870804292
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-03 16:43:17 +0530

    add EmployeeWage UC14

--- src/Day9/IComputeEmpWage.java
+++ src/Day9/IComputeEmpWage.java
@@ -4,4 +4,5 @@ public interface IComputeEmpWage
 {
 	 void addCompanyEmpWage(String CompanyName, int Per_Hour_Salary, int Total_Working_Days,int Total_Working_hours);
 	 void computeEmpWage();
+	 int grtTotalEmpWage(String CompanyName);
 }
